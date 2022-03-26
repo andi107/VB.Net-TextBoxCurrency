451cce60f1beebaa2f81e0f5eec28bdd4ccd3f3e
andi107 <andifirman107@gmail.com>
2022-03-26 23:19:51 +0700

    First Update

---
+++ ENCurency.Designer.vb
@@ -0,0 +1,54 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ENCurency
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtCur = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'txtCur
+        '
+        Me.txtCur.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtCur.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtCur.Location = New System.Drawing.Point(0, 0)
+        Me.txtCur.Name = "txtCur"
+        Me.txtCur.Size = New System.Drawing.Size(151, 20)
+        Me.txtCur.TabIndex = 0
+        Me.txtCur.Text = "0"
+        Me.txtCur.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'ENCurency
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.txtCur)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Name = "ENCurency"
+        Me.Size = New System.Drawing.Size(151, 20)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtCur As System.Windows.Forms.TextBox
+
+End Class
---
+++ ENCurency_L.vb
@@ -0,0 +1,92 @@
+﻿Public Class ENCurency_L
+
+    Public Shadows Event TextChanged(sender As Object, e As EventArgs)
+    Public Shadows Event KeyPress(sender As Object, e As KeyPressEventArgs)
+    Public Sub ENCurency_Replace(ByVal ObjText As Object)
+        With ObjText
+            .Text = Replace(.Text, ".00", "")
+            .Text = Replace(.Text, ",", "")
+            .Text = Replace(.Text, " ", "")
+        End With
+    End Sub
+
+    Public Sub ENCurency_LostFocus(ByVal ObjTextBox As TextBox)
+        Dim f As Double
+        ObjTextBox.TextAlign = HorizontalAlignment.Right
+        If ObjTextBox.Text = "" Or Not IsNumeric(ObjTextBox.Text) Then
+            Exit Sub
+        End If
+        f = CDbl(ObjTextBox.Text)
+        ObjTextBox.Text = Format(f, "##,#0.00")
+        ObjTextBox.SelectionStart = Len(ObjTextBox.Text)
+    End Sub
+
+    Private Sub txtCur_Click(ByVal sender As Object, ByVal e As EventArgs) Handles txtCur.Click
+        ENCurency_Replace(txtCur)
+    End Sub
+
+    Private Sub txtCur_GotFocus(ByVal sender As Object, ByVal e As System.EventArgs) Handles txtCur.GotFocus
+        ENCurency_Replace(txtCur)
+    End Sub
+
+    Private Sub txtCur_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtCur.KeyPress
+        'ENTextBoxNumeric(txtCur, e)
+        RaiseEvent KeyPress(sender, e)
+    End Sub
+
+    Private Sub txtCur_LostFocus(ByVal sender As Object, ByVal e As EventArgs) Handles txtCur.LostFocus
+        If txtCur.Text = "" Then txtCur.Text = Val(0)
+        ENCurency_LostFocus(txtCur)
+    End Sub
+
+    Overrides Property Text As String
+        Get
+            'ENCurency_Replace(txtCur)
+            Text = txtCur.Text
+            'ENCurency_LostFocus(txtCur)
+        End Get
+        Set(ByVal v As String)
+            txtCur.Text = v
+            Invalidate()
+            ENCurency_LostFocus(txtCur)
+        End Set
+    End Property
+
+    Private Sub ENTextBoxNumeric(ByVal ObjText As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs)
+        If Asc(e.KeyChar) <> 8 Then
+            If Asc(e.KeyChar) < 48 Or Asc(e.KeyChar) > 57 Then
+                e.Handled = True
+            End If
+        End If
+    End Sub
+
+    Private Sub ENCurency_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+        'Font = txtCur.Font
+        'Height = txtCur.Height
+    End Sub
+
+    Property TextReadOnly As Boolean
+        Get
+            Return txtCur.ReadOnly
+        End Get
+        Set(value As Boolean)
+            txtCur.ReadOnly = value
+        End Set
+    End Property
+
+    Private Sub txtCur_TextChanged(sender As Object, e As EventArgs) Handles txtCur.TextChanged
+        RaiseEvent TextChanged(sender, e)
+    End Sub
+
+    Public Overrides Property Font As Font
+        Get
+            Return MyBase.Font
+        End Get
+        Set(value As Font)
+            txtCur.Font = value
+            Width = txtCur.Width
+            MyBase.Font = value
+        End Set
+    End Property
+
+End Class
